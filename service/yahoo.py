8c2ba29945ca7de940e40a6912a43026c570f626
ivan1042 <ivan79ch@gmail.com>
2026-07-06 22:56:04 +0800

    creating project, completed yfinance caller

---
+++ service/yahoo.py
@@ -0,0 +1,19 @@
+import yfinance as yf
+import datetime
+import shutil
+from pathlib import Path
+
+def get_historical_data(symbol, start = None, end = datetime.date.today().strftime('%Y-%m-%d'), interval = "1mo"):
+    if start == None:
+        data = yf.download(symbol, period = "max", interval = interval)
+    else:
+        data = yf.download(symbol, start = start, end = end, interval = interval)
+
+    data.to_csv(f"{symbol}_historical.csv")
+    service = Path(__file__).resolve().parent
+    file = service / f"{symbol}_historical.csv"
+    target = service.parent / "data"
+    shutil.move(file, target)
+
+
+get_historical_data("AAPL")
