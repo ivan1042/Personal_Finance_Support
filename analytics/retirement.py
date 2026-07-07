c847cd251b8093e08de76b9a5faee3c7c8cb2b53
ivan1042 <ivan79ch@gmail.com>
2026-07-07 19:32:13 +0800

    current value at retirement age formula

---
+++ analytics/retirement.py
@@ -0,0 +1,14 @@
+from analytics.returns import *
+from analytics.risk import *
+
+def retirement_amount(retire_age = 65, death_age = 100, monthly_expense = 8681, yearly_inflation = 0.03):
+    period = (death_age - retire_age) * 12
+    monthly_return = monthly_geo_mean - 1
+    monthly_inflation = (1 + yearly_inflation) ** (1/12) - 1
+
+    cash = monthly_expense / (monthly_return - monthly_inflation)
+    ratio = (1 - ((1 + monthly_inflation )/ (1 + monthly_return )) ** period)
+
+    retirement_amount = cash * ratio
+    return f"{retirement_amount:.0f}"
+
--- analytics/risk.py
+++ analytics/risk.py
@@ -7,14 +7,14 @@ risk_free = 0.045
 
 #No assumption
 historical_VaR = np.percentile(df["%Change"], (1 - confidence_level) * 100)
-print(f"Historical VaR (95% Confidence): {historical_VaR:.2%}")
+#print(f"Historical VaR (95% Confidence): {historical_VaR:.2%}")
 
 #Assume normal distribution
 mu = np.mean(df["%Change"])
 sigma = np.std(df["%Change"])
 z_score = stats.norm.ppf(1 - confidence_level)
 parametric_VaR = mu - z_score * sigma
-print(f"Parametric VaR (95% Confidence): {parametric_VaR:.2%}")
+#print(f"Parametric VaR (95% Confidence): {parametric_VaR:.2%}")
 
 
 
