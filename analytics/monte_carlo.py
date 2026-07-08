5cfbd540e0cd6012770e5f46eface8db1d268703
ivan1042 <ivan79ch@gmail.com>
2026-07-08 23:07:21 +0800

    dashbroad data flow all sorted out, monte_carlo bug still hanging

--- analytics/monte_carlo.py
+++ analytics/monte_carlo.py
@@ -4,26 +4,27 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
-df, monthly_art_mean, yearly_art_mean, monthly_geo_mean, yearly_geo_mean = returns.stats()
-stock = ["VFINX", "AAPL"]
 max_sim = 100
-time = (100 - 65) * 12
-initial_amount = 10000
-weight = np.array([0.5, 0.5]).T
-meanM = np.full(shape = (time, len(weight)), fill_value = monthly_geo_mean)
-meanM = meanM.T
-portfolio_sim = np.full(shape = (time, max_sim), fill_value = 0.0)
-if len(stock) <= 1:
-    cov_matrix = np.array([[risk.volatility ** 2]])
-else:
-    return_df = df["Ratio"]
-    cov_matrix = return_df.cov()
+def simulation(raw_mon_return_temp, mon_re_temp, stock = ["VFINX", "AAPL"], weight = [0.7, 0.3],
+               retire_age = 65, death_age = 100):
+    return_df = pd.concat(raw_mon_return_temp, axis=1)
+    return_matrix = np.array(mon_re_temp).T
+    time = (death_age - retire_age) * 12
+    initial_amount = 10000
+    weight = np.array(weight).T
+    meanM = np.full(shape = (time, len(weight)), fill_value = return_matrix)
+    meanM = meanM.T
+    portfolio_sim = np.full(shape = (time, max_sim), fill_value = 0.0)
+    if len(stock) <= 1:
+        cov_matrix = np.array(return_df ** 2)
+    else:
+        cov_matrix = return_df.cov()
 
-for m in range(0, max_sim):
-    Z = np.random.normal(size = (time, len(weight)))
-    L = np.linalg.cholesky(cov_matrix)
-    monthly_return = meanM + np.inner(L, Z)
-    portfolio_sim[: ,m] = np.cumprod(np.inner(weight, monthly_return.T + 1)) * initial_amount
+    for m in range(0, max_sim):
+        Z = np.random.normal(size = (time, len(weight)))
+        L = np.linalg.cholesky(cov_matrix)
+        monthly_return = meanM + np.inner(L, Z)
+        portfolio_sim[: ,m] = np.cumprod(np.inner(weight, monthly_return.T + 1)) * initial_amount
 
-plt.plot(portfolio_sim)
-plt.show()
+    plt.plot(portfolio_sim)
+    plt.show()
--- dashbroad.py
+++ dashbroad.py
@@ -1,19 +1,29 @@
+import pandas as pd
+import numpy as np
 from analytics import risk
 from analytics import returns
 from analytics import retirement
+from analytics import monte_carlo
 
-stock = ["VFINX", "AAPL"]
+stocks = ["VFINX", "AAPL"]
+weight = [0.7, 0.3]
 data = []
-
-for stock in stock:
+raw_mon_return_temp = []
+geo_return_temp = []
+for stock in stocks:
     data.append(returns.stats(stock))
+
+for k in range(0, len(stocks)):
+    raw_mon_return_temp.append(data[k][0]["%Change"])
+    geo_return_temp.append(data[k][3])
 
 
 for k in data:
     print(risk.risk_calc(*k))
-
 for k in data:
     print(retirement.retirement_amount(k[3]))
+
+monte_carlo.simulation(raw_mon_return_temp, geo_return_temp, stocks, weight )
 
 """historical_VaR = risk.historical_VaR
 print(returns.monthly_geo_mean)
