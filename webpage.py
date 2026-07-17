87e99f0399a868573fbd75738bd5af0f14dc79fd
ivan1042 <ivan79ch@gmail.com>
2026-07-17 12:29:36 +0800

    started building dashborad, but the design progress is not ideal

---
+++ webpage.py
@@ -0,0 +1,7 @@
+import streamlit as st
+import plotly.express as px
+
+st.set_page_config(page_title = "Finance Dashboard", page_icon = ":bar_chart:", layout="wide")
+st.title("Welcome to Finance Dashboard")
+st.markdown('<style>div.block-container{padding-top:1rem;}</style>', unsafe_allow_html=True)
+
