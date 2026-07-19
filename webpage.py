b7a8a94ead09d5aca4feba97208f49584df27dfb
ivan1042 <ivan79ch@gmail.com>
2026-07-19 22:59:06 +0800

    waiting to make filter

--- webpage.py
+++ webpage.py
@@ -6,5 +6,7 @@ st.set_page_config(page_title = "Finance Dashboard", page_icon = ":bar_chart:", 
 st.subheader("Welcome to Finance Dashboard")
 st.markdown("##", unsafe_allow_html=True)
 
+df_ticker_info = pd.DataFrame(ticker_info, columns=["Ticker", "Close", "Industry", "Summary"])
+df_ticker_info.set_index(df_ticker_info.columns[0], inplace=True)
 st.dataframe(df_ticker_info)
 st.markdown("##", unsafe_allow_html=True)
