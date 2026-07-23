289908fd96d7a8c40a3096dd1593ad0880f51eca
ivan1042 <ivan79ch@gmail.com>
2026-07-23 19:29:02 +0800

    versioned appl rolling volatility and beta, but timeframe is not clear

--- Stock analysis/parameter.py
+++ Stock analysis/parameter.py
@@ -1,8 +1,21 @@
 import lazy_update
-
-stocks = ["AAPL", "VFINX"]
+import matplotlib.pyplot as plt
+"""stocks = ["AAPL", "VFINX"]
 
 for stock in stocks:
     lazy_update.csv_checker(stock)
     print(lazy_update.to_dataframe(stock))
 
+"""
+
+df_AAPL = lazy_update.to_dataframe("AAPL")
+df_VFINX = lazy_update.to_dataframe("VFINX")
+df_AAPL["Volatility"] = df_AAPL['%Change'].rolling(window=36).std()
+df_AAPL["Beta"] = df_AAPL["%Change"].rolling(window=36).cov(df_VFINX["%Change"]) / (df_AAPL["Volatility"]) ** 2
+
+print(df_AAPL)
+df_AAPL.drop(columns = ["Close", "Change", "Ratio"], inplace = True)
+for col in df_AAPL:
+    plt.plot(df_AAPL[col], label = col)
+    plt.legend()
+plt.show()
