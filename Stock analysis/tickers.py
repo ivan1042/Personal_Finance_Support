fef09ab80b40fccb64807bd5fbbec83f9422ee3f
ivan1042 <ivan79ch@gmail.com>
2026-07-30 17:15:44 +0800

    correctly functioned

---
+++ Stock analysis/tickers.py
@@ -0,0 +1,48 @@
+import streamlit as st
+import plotly.express as px
+from plotly.subplots import make_subplots
+from parameter import *
+
+
+target = ["NVDA", "AAPL", "MSFT", "GOOG", "AMZN"]
+interval= "1d"
+
+def plot(df, target):
+    st.subheader(f"{target} close")
+    fig_1 = make_subplots()
+    fig_1_close = px.line(df["Close"])
+    fig_1_top_vo = px.scatter(df["Top_vo"])
+    fig_1_top_be = px.scatter(df["Top_be"])
+    fig_1_buttom_vo = px.scatter(df["Buttom_vo"])
+    fig_1_buttom_be = px.scatter(df["Buttom_be"])
+    fig_1_top_vo.update_traces(marker=dict(color="green"))
+    fig_1_top_be.update_traces(marker=dict(color="red"))
+    fig_1_buttom_vo.update_traces(marker=dict(color="blue"))
+    fig_1_buttom_be.update_traces(marker=dict(color="orange"))
+
+    fig_1.add_traces(
+        fig_1_close.data + fig_1_top_vo.data + fig_1_top_be.data + fig_1_buttom_vo.data + fig_1_buttom_be.data)
+
+    st.plotly_chart(
+        fig_1,
+        width='stretch'
+    )
+
+left, right = st.columns([1,1])
+st.set_page_config(layout="wide")
+df_VFINX = VFINX_df(interval)
+
+for i, k in enumerate(target):
+    df = stock_stats(k, interval)
+    print(df)
+    if i % 2 == 0:
+        with left:
+            plot(df, k)
+    else:
+        with right:
+            plot(df, k)
+
+
+
+
+
